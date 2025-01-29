5cd8c806719043d6d92955dbbb5f13a6b487ba3a
codythompson <cody.thompson22@gmail.com>
2025-01-29 14:37:23 -0800

    version 0.0.5 did not have the latest css in the lit version

--- build.zsh
+++ build.zsh
@@ -1,3 +1,4 @@
+echo "[build.zsh] deleting existing dist folder"
 rm -rf ./dist
 mkdir ./dist
 
@@ -9,9 +10,12 @@ mkdir -p dist/css
 mkdir -p dist/lit
 
 # compiles the CSS file
+echo "[build.zsh] compiling .less to .css with lessc"
 npx lessc $LOFI_LESS_INDEX $LOFI_CSS_OUTPUT
 
 # embeds the contents of the css file inside a call to the lit css wrapper
+echo "[build.zsh] embedding css in lit .ts file"
 printf 'import { css } from "lit";\n\nexport default css`\n' > "$LOFI_LIT_OUTPUT" &&
 cat "$LOFI_CSS_OUTPUT" >> "$LOFI_LIT_OUTPUT" &&
 printf '`;\n' >> "$LOFI_LIT_OUTPUT";
+echo "[build.zsh] done"
